4e9a7d3aabac265451d97932ba9c376d55a17e64
Matthias Reining <matthias.reining@outlook.com>
2015-10-23 15:51:59 +0200

    Web Listener (session expired)

---
+++ src/main/java/de/fhws/app/listener/SessionListener.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.fhws.app.listener;
+
+import de.fhws.app.entity.AppUser;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+@WebListener
+public class SessionListener implements HttpSessionListener {
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        System.out.println("session created");
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        AppUser user = (AppUser) se.getSession().getAttribute("user");
+        if (user != null)
+            System.out.println("session destroyed for " + user.getFirstName());
+        else
+            System.out.println("session destroyed for anonymous");
+
+    }
+
+}
