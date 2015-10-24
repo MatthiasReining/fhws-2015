bf05561c50b0ca1f34e4c1a960173a852aa30297
Matthias Reining <matthias.reining@outlook.com>
2015-10-24 14:33:47 +0200

    JSF Templates (WEB-INF/templates/template.xhtml)

---
+++ src/main/java/de/fhws/app/presentation/SampleController.java
@@ -0,0 +1,28 @@
+package de.fhws.app.presentation;
+
+import de.fhws.app.business.usermanagement.entity.AppUser;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class SampleController {
+
+    private String data1;
+    private String data2;
+
+    public String getData1() {
+        return data1;
+    }
+
+    public void setData1(String data1) {
+        this.data1 = data1;
+    }
+
+    public String getData2() {
+        return data2;
+    }
+
+    public void setData2(String data2) {
+        this.data2 = data2;
+    }
+
+}
